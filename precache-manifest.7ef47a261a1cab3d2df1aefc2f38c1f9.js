56d39ebe4e5e95767fc4677c152d98cd4f410e1b
ReD-GuardiaN <60849587+ReD-GuardiaN@users.noreply.github.com>
2022-01-28 13:25:05 +0200

    Add files via upload

---
+++ precache-manifest.7ef47a261a1cab3d2df1aefc2f38c1f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f23aa1284a449cbb474e840c58207c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "322d588c14c198d42aad",
+    "url": "/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "9f16669a78388b1c7e2d",
+    "url": "/static/js/2.479629ae.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.479629ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "322d588c14c198d42aad",
+    "url": "/static/js/main.7bebfb74.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
